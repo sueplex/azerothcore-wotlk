0ac956ea97166f0f51e6032e56d104501c9bc978
Sage <abrahamsage@gmail.com>
2024-07-20 12:57:34 -0400

    Database Secrets

--- apps/docker/entrypoint.sh
+++ apps/docker/entrypoint.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+#!/bin/sh
+
+# Loop through all environment variables
+for var in $(env | grep '_SECRET_FILE=' | sed 's/=.*//'); do
+    # Read the file path from the environment variable
+    file_path=$(printenv $var)
+
+    # Check if the file exists
+    if [ -f "$file_path" ]; then
+        # Read the content of the file
+        file_content=$(cat "$file_path")
+
+        # Create a new environment variable name by removing the '_FILE' suffix
+        new_var=$(echo $var | sed 's/_SECRET_FILE$//')
+
+        # Export the new environment variable with the content of the file
+        export $new_var="$file_content"
+    else
+        echo "File $file_path does not exist."
+    fi
+done
+
 CONF_DIR="${CONF_DIR:-/azerothcore/env/dist/etc}"
 LOGS_DIR="${LOGS_DIR:-/azerothcore/env/dist/logs}"
 
